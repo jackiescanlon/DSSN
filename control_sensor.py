22a9016b61e8a46b2a8347d1ddfea2339049db3c
Jackie <scanlj2@rpi.edu>
2019-01-31 21:48:09 -0500

    Modified the IP

--- control_sensor.py
+++ control_sensor.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('129.161.136.200', 20001)
+server_address = ('192.168.1.198', 10001)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
