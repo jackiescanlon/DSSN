f5da3fb40864278818e212f52b05b99e11acb5a0
Jackie - Rasp Pi <scanlj2@rpi.edu>
2019-01-31 21:45:32 -0500

    Testing the TCP connection w/ camera

--- control_camera.py
+++ control_camera.py
@@ -8,7 +8,7 @@ from picamera import PiCamera
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('129.161.136.200', 10000)  #Port 1000 (can be changed), and the IP address can chnge
+server_address = ('192.168.1.198', 10001)  #Port 1000 (can be changed), and the IP address can chnge
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
@@ -26,14 +26,14 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(1024)
-            print('received {!r}'.format(data))
             if data:
+                print('received {!r}'.format(data))
                 print('Taking a picture')
                 camera.start_preview()
                 sleep(2) # Camera warm up time
                 date = datetime.datetime.now()
                 date_string = date.strftime("%m_%d_%Y_%H_%M_%S")
-                camera.capture('~/camera_captures/' + date_string + '.jpg')
+                camera.capture('/home/pi/Documents/DSSN/Pictures/' + date_string + '.jpg')
                 print('Done taking picture')
             else:
                 print('no data from', client_address)
