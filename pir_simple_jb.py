72cc7a68942aa1f1de2343f7024b2911f3872f7e
Jeremy Bornstein <jbor34@gmail.com>
2019-02-03 16:27:31 -0500

    Basic PIR dection code - hopefully it works

---
+++ pir_simple_jb.py
@@ -0,0 +1,21 @@
+#Basic code to control the PIR Sensor
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False) #if RPi.GPIO detects
+GPIO.setmode(GPIO.BOARD)
+
+#Read output pin of PIR sensor
+GPIO.setup (3,GPIO.IN) #Basic structure GPIO.setup (channel, GPIO.IN/OUT, initial=GPIO.HIGH/LOW)
+
+try:
+  time.sleep(60) #Allows sensor to stabilize (takes about a minute)
+  while True:
+      detection = GPIO.input(3) #read the value of a GPIO pi
+      if detection==1:                 #When output from motion sensor is LOW
+        print ("Intruder alert")
+        print ("Who's man's is in my house")
+        time.sleep(2) #Adjust this as necessary to get rid of multipl detections
+      time.sleep(.01)
+except:
+  GPIO.cleanup()
